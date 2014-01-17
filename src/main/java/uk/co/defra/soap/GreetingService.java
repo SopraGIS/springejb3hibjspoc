c0d3077e393ba2f5ebf1ab1176af81f2b7e31136
gautirao <goutham@jbits.info>
2014-01-17 16:24:50 +0000

    another simple service

---
+++ src/main/java/uk/co/defra/soap/GreetingService.java
@@ -0,0 +1,18 @@
+package uk.co.defra.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class GreetingService {
+
+	private String message = new String("Hello, ");
+
+    public void Hello() {
+    }
+
+    @WebMethod
+    public String sayHello(String name) {
+        return message + name + ".";
+    }
+}
